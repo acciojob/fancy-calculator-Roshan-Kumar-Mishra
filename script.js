072a102934beda10880c01ac85ab06f0ba42cae5
Roshan-Kumar-Mishra <144347812+Roshan-Kumar-Mishra@users.noreply.github.com>
2023-12-15 18:56:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+// Take input for the first variable and multiply it by 2
+const firstInput = prompt("Enter the first variable:");
+const multipliedResult = parseFloat(firstInput) * 2;
+
+// Take input for the second variable
+const secondInput = prompt("Enter the second variable:");
+
+// Add the second input to the result of the first input multiplied by 2
+const finalResult = multipliedResult + parseFloat(secondInput);
+
+// Display the final result using alert()
+alert("The final result is: " + finalResult);
